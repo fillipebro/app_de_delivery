eaaf6f60b905cc18749c02c22fcdd6f6448554e2
Fillipe Ribeiro <rfillipe21@gmail.com>
2025-09-09 13:09:22 -0300

    Create OrderRepository.java

---
+++ src/main/java/com/ecotech/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.ecotech.repository;
+
+import com.ecotech.model.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+}
