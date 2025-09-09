591cdb5d6a947028c4e4e5ba4b1d35b97a4a1c33
Fillipe Ribeiro <rfillipe21@gmail.com>
2025-09-09 13:08:58 -0300

    Create EcotechBackendApplication.java

---
+++ src/main/java/com/ecotech/EcotechBackendApplication.java
@@ -0,0 +1,11 @@
+package com.ecotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTechBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcoTechBackendApplication.class, args);
+    }
+}
